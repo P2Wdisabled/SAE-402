dc5a014d6e63145bd2bb61c16749956e1f26c960
Louis <louis1.potevin1@gmail.com>
2025-02-27 09:11:14 +0100

    adding missions to defaultDatas

--- datas/defaultDatas.js
+++ datas/defaultDatas.js
@@ -170,6 +170,17 @@ let questionsNPC4 = [
     };
 
     let score = 0;
+    let missions = [
+        { id: 1, text: "Talk to Steve (Spawn NPC)", completed: false },
+        { id: 2, text: "Talk to Greg", completed: false },
+        { id: 3, text: "Ask Greg food", completed: false },
+        { id: 4, text: "Talk to Mickael", completed: false },
+        { id: 5, text: "Speak about Business with Mickael", completed: false },
+        { id: 6, text: "Talk to Luke", completed: false },
+        { id: 7, text: "Ask about New York Hobbies to Luke", completed: false },
+        { id: 8, text: "Talk to Jack", completed: false },
+        { id: 9, text: "Speak about tourism with Jack", completed: false }
+    ];
 
 export const defaultData = {
     score: score,
@@ -180,5 +191,6 @@ export const defaultData = {
     questionsNPC2: questionsNPC2,
     questionsNPC3: questionsNPC3,
     questionsNPC4: questionsNPC4,
+    missions: missions,
     time: 600
 };
