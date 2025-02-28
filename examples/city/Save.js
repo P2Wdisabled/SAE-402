3d62490b531e487c3cdb49a524268996ca61f3df
Louis <175369104+P2Wdisabled@users.noreply.github.com>
2025-02-28 13:40:23 +0100

    patch missing elements

--- examples/city/Save.js
+++ examples/city/Save.js
@@ -1,3 +1,11 @@
+import { updateMissionData } from "./data/MissionData.js";
+import {setTimeRemaining, getTimeRemaining} from "./Timer/ModelTimer.js";
+import {updateTimer} from "./Timer/viewTimer.js";
+import {setscore, getScore} from "./Score/ModelScore.js";
+import {updateScore} from "./Score/viewScore.js";
+import {getMissions} from "./Mission/ModelMission.js"
+import {updateMissionDisplay} from "./Mission/ViewMission.js"
+import {remainingQuestions, questionsNPC1, questionsNPC2, questionsNPC3, questionsNPC4, dialogues, setDatas} from "./data/QuestionData.js";
 export async function loadPlayerThings() {
     //load player data from local storage using the stored "saveId" that define the save to use
     let storedData = localStorage.getItem('Save' + JSON.parse(localStorage.getItem('SaveId')));
@@ -5,24 +13,37 @@ export async function loadPlayerThings() {
     if (!storedData) return;
     //parse the data to a JSON object
     let data = JSON.parse(storedData);
+    updateMissionData(data.mission)
     //return the parsed data
-    return data;
+    setDatas(data.questionsNPC1, data.questionsNPC2, data.questionsNPC3, data.questionsNPC4, data.dialogues, data.remainingQuestions)
+    setscore(data.score);
+    updateScore(data.score);
+    updateMissionDisplay(data.missions);
+    setTimeRemaining(data.time);
+    updateTimer(data.time)
+    document.getElementById('rig').setAttribute('position', data.position);
 }
 
 //function to save the player's data to local storage
-export async function savePlayerThings(remainingQuestions, questionsNPC1, questionsNPC2, questionsNPC3, questionsNPC4, dialogues, missions, timeRemaining, score, position) {
+export async function savePlayerThings() {
     const playerData = {
-        score: score,
-        position: position,
+        score: getScore(),
+        position: document.getElementById('rig').getAttribute('position'),
         remainingQuestions: remainingQuestions,
         dialogues: dialogues,
-        missions: missions,
+        missions: getMissions(),
         questionsNPC1: questionsNPC1,
         questionsNPC2: questionsNPC2,
         questionsNPC3: questionsNPC3,
         questionsNPC4: questionsNPC4,
-        time: timeRemaining
+        time: getTimeRemaining()
     };
 
     localStorage.setItem('Save' + JSON.parse(localStorage.getItem('SaveId')), JSON.stringify(playerData));
+}
+
+export async function startSaving() {
+    setInterval(async () => {
+        await savePlayerThings();
+    }, 1000);
 }
